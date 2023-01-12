1c6e231e3756643368c8840e555a1157d5495c75
Ghayah Almadi <gsalmadi22@twq.idserve.net>
2023-01-12 12:14:07 +0300

    Initial Commit

---
+++ Narrative/NarrativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarrativeApp.swift
+//  Narrative
+//
+//  Created by Ghayah Almadi on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NarrativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
